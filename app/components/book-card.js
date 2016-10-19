6a19b7ad1594d5c920158d13c372f81e4ce8049b
GioBakradze <gio.bakradze.92@gmail.com>
2016-10-19 20:47:59 +0400

    added first click handler

--- app/components/book-card.js
+++ app/components/book-card.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    actions: {
+        zoomCard: function () {
+            console.log('zoom actioon :D');
+        }
+    }
 });
