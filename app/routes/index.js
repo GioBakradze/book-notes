686651d13833680bdd1eed3f899a7b20f4cc6f03
GioBakradze <gio.bakradze.92@gmail.com>
2016-10-19 20:16:31 +0400

    skeleton

---
+++ app/routes/index.js
@@ -0,0 +1,70 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    model() {
+
+        return [
+            {
+                title: 'Deep Work',
+                notes: [
+                    'this book is so inspiring',
+                    'i want to use this all the time'
+                ]
+            },
+            {
+                title: 'Periodic Fasting',
+                notes: [
+                    'Very useful tips about fasting',
+                    'Implement IF'
+                ]
+            },
+            {
+                title: 'JS Up & Going',
+                notes: [
+                    'Purchase other books from this series',
+                    'Start reading professional books',
+                    'Start implementing pet projects'
+                ]
+            },
+            {
+                title: 'Periodic Fasting',
+                notes: [
+                    'Very useful tips about fasting',
+                    'Implement IF'
+                ]
+            },
+            {
+                title: 'JS Up & Going',
+                notes: [
+                    'Purchase other books from this series',
+                    'Start reading professional books',
+                    'Start implementing pet projects'
+                ]
+            },
+            {
+                title: 'Periodic Fasting',
+                notes: [
+                    'Very useful tips about fasting',
+                    'Implement IF'
+                ]
+            },
+            {
+                title: 'JS Up & Going',
+                notes: [
+                    'Purchase other books from this series',
+                    'Start reading professional books',
+                    'Start implementing pet projects'
+                ]
+            },
+            {
+                title: 'JS Up & Going',
+                notes: [
+                    'Purchase other books from this series',
+                    'Start reading professional books',
+                    'Start implementing pet projects'
+                ]
+            }
+        ];
+
+    }
+});
